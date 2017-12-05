eff04bb51c0a1567705749dc32ca7686f7d56fca
tianl9528 <tianl9528@users.noreply.github.com>
2017-12-06 01:23:44 +0800

    add UsersListActivity

--- app/src/main/java/com/tianl9528/loginwithsqlite/activities/UsersListActivity.java
+++ app/src/main/java/com/tianl9528/loginwithsqlite/activities/UsersListActivity.java
@@ -1,8 +1,22 @@
 package com.tianl9528.loginwithsqlite.activities;
 
-/**
- * Created by tianl9528 on 2017/12/6.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-class UsersListActivity {
+import com.tianl9528.loginwithsqlite.R;
+
+public class UsersListActivity extends AppCompatActivity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userslist);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
